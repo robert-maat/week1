e0eacbfdbe0cb84bc28cfb171a7f237b7f7c9f8d
Robert <robert.maat@gmail.com>
2020-05-24 06:58:34 -0700

    devconn.py created

---
+++ devconn.py
@@ -0,0 +1,31 @@
+import os
+from netmiko import ConnectHandler
+from getpass import getpass
+
+homedir = os.path.expanduser('~')
+filename = ".cred" 
+credfile = homedir + "/" + filename
+
+with open(credfile) as f:
+    cred = f.read()
+
+username, password = cred.split(":")
+username = username.strip()
+password = password.strip()
+
+nxos1 = {
+    "device_type": "cisco_nxos",
+    "host": "nxos1.lasthop.io",
+    "username": username, 
+    "password": password, 
+    "port": "22",
+}
+
+conDev = ConnectHandler(**nxos1)
+
+print(conDev.find_prompt())
+
+
+
+
+
